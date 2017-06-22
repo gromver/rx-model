4590511c29341c041c22efb39705c90dada6439b
Gayazov Roman <gromver5@gmail.com>
2017-06-22 18:07:59 +0300

    remove promise-any

--- src/validators/MultiValidator.js
+++ src/validators/MultiValidator.js
@@ -1,5 +1,12 @@
 import Validator from './Validator';
-import promiseAny from 'promise-any';
+
+const reverse = (promise) => {
+  return new Promise((resolve, reject) => Promise.resolve(promise).then(reject, resolve));
+};
+
+const promiseAny = (iterable) => {
+  return reverse(Promise.all([...iterable].map(reverse)));
+};
 
 export default class MultiValidator extends Validator {
   validators;
