9a388b0301381c2729f13ab15088ae2d3501d3fa
Gayazov Roman <gromver5@gmail.com>
2017-08-22 14:45:46 +0300

    cleanup utils and add tests

--- src/tests/Utils.test.js
+++ src/tests/Utils.test.js
@@ -1,7 +1,317 @@
+import moment from 'moment';
 import utils from '../utils';
 
-describe('Test utils.js', () => {
-  test('resolveAttribute()', () => {
+describe('isNumber', function() {
+  test("returns true for numbers", function() {
+    expect(utils.isNumber(0)).toBe(true);
+    expect(utils.isNumber(1)).toBe(true);
+    expect(utils.isNumber(Math.PI)).toBe(true);
+  });
+
+  test("returns false for non numbers", function() {
+    expect(utils.isNumber(null)).toBe(false);
+    expect(utils.isNumber(true)).toBe(false);
+    expect(utils.isNumber("1")).toBe(false);
+  });
+});
+
+describe('isInteger', function() {
+  test("returns true for integers", function() {
+    expect(utils.isInteger(0)).toBe(true);
+    expect(utils.isInteger(1)).toBe(true);
+  });
+
+  test("returns false for floats and other types ", function() {
+    expect(utils.isInteger(Math.PI)).toBe(false);
+    expect(utils.isInteger(null)).toBe(false);
+    expect(utils.isInteger("1")).toBe(false);
+  });
+});
+
+describe('isBoolean', function() {
+  test("returns true for booleans", function() {
+    expect(utils.isBoolean(true)).toBe(true);
+    expect(utils.isBoolean(false)).toBe(true);
+  });
+
+  test("returns false for non booleans ", function() {
+    expect(utils.isBoolean(null)).toBe(false);
+    expect(utils.isBoolean({})).toBe(false);
+    expect(utils.isBoolean({foo: "bar"})).toBe(false);
+    expect(utils.isBoolean([])).toBe(false);
+    expect(utils.isBoolean("")).toBe(false);
+    expect(utils.isBoolean(function() {})).toBe(false);
+  });
+});
+
+describe('isObject', function() {
+  test("returns true for objects", function() {
+    expect(utils.isObject({})).toBe(true);
+    expect(utils.isObject({foo: "bar"})).toBe(true);
+    expect(utils.isObject([])).toBe(true);
+    expect(utils.isObject(function() {})).toBe(true);
+  });
+
+  test("returns false for non objects", function() {
+    expect(utils.isObject(null)).toBe(false);
+    expect(utils.isObject(1)).toBe(false);
+    expect(utils.isObject("")).toBe(false);
+    expect(utils.isObject(false)).toBe(false);
+  });
+});
+
+describe('isDefined', function() {
+  test("returns false for null and undefined", function() {
+    expect(utils.isDefined(null)).toBe(false);
+    expect(utils.isDefined(undefined)).toBe(false);
+  });
+
+  test("returns true for other values", function() {
+    expect(utils.isDefined(true)).toBe(true);
+    expect(utils.isDefined(0)).toBe(true);
+    expect(utils.isDefined("")).toBe(true);
+  });
+});
+
+describe("isPromise", function() {
+  test("returns false for null and undefined", function() {
+    expect(utils.isPromise(null)).toBe(false);
+    expect(utils.isPromise(undefined)).toBe(false);
+  });
+
+  test("returns false for objects", function() {
+    expect(utils.isPromise({})).toBe(false);
+  });
+
+  test("returns true for objects with a then function", function() {
+    expect(utils.isPromise({then: "that"})).toBe(false);
+    expect(utils.isPromise({then: function() {}})).toBe(true);
+  });
+});
+
+describe('isString', function() {
+  test("returns true for strings", function() {
+    expect(utils.isString("foobar")).toBe(true);
+    expect(utils.isString("")).toBe(true);
+  });
+
+  test("returns false for non strings", function() {
+    const obj = {toString: function() { return "foobar"; }};
+    expect(utils.isString(obj)).toBe(false);
+    expect(utils.isString(null)).toBe(false);
+    expect(utils.isString(true)).toBe(false);
+  });
+});
+
+describe('isArray', function() {
+  const isArray = utils.isArray;
+
+  test("returns true for arrays", function() {
+    expect(isArray([])).toBe(true);
+    expect(isArray([1])).toBe(true);
+    expect(isArray([1, 2])).toBe(true);
+  });
+
+  test("returns false for non arrays", function() {
+    expect(isArray({})).toBe(false);
+    expect(isArray(null)).toBe(false);
+    expect(isArray(1)).toBe(false);
+    expect(isArray(true)).toBe(false);
+  });
+});
+
+describe('isHash', function() {
+  test("returns true for hashes", function() {
+    expect(utils.isHash({})).toBe(true);
+    expect(utils.isHash({foo: "bar"})).toBe(true);
+  });
+
+  test("returns false for non hashes", function() {
+    expect(utils.isHash([])).toBe(false);
+    expect(utils.isHash(function() {})).toBe(false);
+    expect(utils.isHash(null)).toBe(false);
+    expect(utils.isHash(1)).toBe(false);
+    expect(utils.isHash("")).toBe(false);
+    expect(utils.isHash(false)).toBe(false);
+  });
+});
+
+describe('isFunction', function() {
+  const isFunction = utils.isFunction;
+
+  test("returns true for functions", function() {
+    expect(isFunction(function() {})).toBe(true);
+  });
+
+  test("returns false for non functions", function() {
+    expect(isFunction({})).toBe(false);
+    expect(isFunction(null)).toBe(false);
+    expect(isFunction(1)).toBe(false);
+    expect(isFunction(true)).toBe(false);
+  });
+});
+
+describe("isDate", function() {
+  test("returns true for dates", function() {
+    expect(utils.isDate(new Date())).toBe(true);
+  });
+
+  test("returns false for non dates", function() {
+    expect(utils.isDate(Date.now())).toBe(false);
+    expect(utils.isDate({})).toBe(false);
+  });
+});
+
+describe("isMoment", function() {
+  test("returns true for moments", function() {
+    expect(utils.isMoment(moment())).toBe(true);
+  });
+
+  test("returns false for non moments", function() {
+    expect(utils.isMoment(new Date())).toBe(false);
+    expect(utils.isMoment({})).toBe(false);
+  });
+});
+
+describe("isEmpty", function() {
+  test("considers null and undefined values empty", function() {
+    expect(utils.isEmpty(null)).toBe(true);
+    expect(utils.isEmpty(undefined)).toBe(true);
+  });
+
+  test("considers functions non empty", function() {
+    expect(utils.isEmpty(function(){})).toBe(false);
+  });
+
+  test("considers whitespace only strings empty", function() {
+    expect(utils.isEmpty("")).toBe(true);
+    expect(utils.isEmpty(" ")).toBe(true);
+    expect(utils.isEmpty("         ")).toBe(true);
+    expect(utils.isEmpty("foo")).toBe(false);
+  });
+
+  test("considers empty arrays empty", function() {
+    expect(utils.isEmpty([])).toBe(true);
+    expect(utils.isEmpty([1])).toBe(false);
+  });
+
+  test("considers empty objects empty", function() {
+    expect(utils.isEmpty({})).toBe(true);
+    expect(utils.isEmpty({foo: "bar"})).toBe(false);
+  });
+
+  test("considers false and 0 non empty", function() {
+    expect(utils.isEmpty(false)).toBe(false);
+    expect(utils.isEmpty(0)).toBe(false);
+  });
+
+  test("considers date non empty", function() {
+    const _utils = { ...utils };
+
+    expect(_utils.isEmpty(new Date())).toBe(false);
+
+    _utils.isDate = jest.fn();
+    _utils.isEmpty(new Date());
+
+    expect(_utils.isDate).toHaveBeenCalled();
+  });
+});
+
+describe("isDomElement", function() {
+  test("returns true of DOM elements", function() {
+    const form = document.createElement("form")
+      , div = document.createElement("div")
+      , a = document.createElement("a");
+
+    expect(utils.isDomElement(form)).toBe(true);
+    expect(utils.isDomElement(div)).toBe(true);
+    expect(utils.isDomElement(a)).toBe(true);
+    expect(utils.isDomElement(document)).toBe(true);
+  });
+
+  test("returns false for other objects", function() {
+    expect(utils.isDomElement({})).toBe(false);
+    expect(utils.isDomElement(0)).toBe(false);
+    expect(utils.isDomElement(true)).toBe(false);
+    expect(utils.isDomElement("foo")).toBe(false);
+    expect(utils.isDomElement("")).toBe(false);
+    expect(utils.isDomElement([])).toBe(false);
+  });
+});
+
+describe('contains', function() {
+  test("returns false when not passing in a target object", function() {
+    expect(utils.contains(null, "foo")).toBe(false);
+    expect(utils.contains(undefined, "foo")).toBe(false);
+  });
+
+  describe("arrays", function() {
+    test("returns true if the value is in the specified array", function() {
+      expect(utils.contains(["foo", "bar", "baz"], "foo")).toBe(true);
+      expect(utils.contains(["foo", "bar", "baz"], "bar")).toBe(true);
+      expect(utils.contains(["foo", "bar", "baz"], "baz")).toBe(true);
+    });
+
+    test("returns false if the value is not in the specified array", function() {
+      expect(utils.contains(["foo", "bar", "baz"], "quux")).toBe(false);
+      expect(utils.contains(["foo", "bar", "baz"], false)).toBe(false);
+      expect(utils.contains(["foo", "bar", "baz"], 0)).toBe(false);
+      expect(utils.contains(["foo", "bar", "baz"], null)).toBe(false);
+    });
+
+    test("works with empty arrays", function() {
+      expect(utils.contains([], "foo")).toBe(false);
+    });
+  });
+
+  describe("objects", function() {
+    test("returns true if the value is a key in the object", function() {
+      expect(utils.contains({foo: false, bar: "bar"}, "foo")).toBe(true);
+      expect(utils.contains({foo: false, bar: "bar"}, "bar")).toBe(true);
+    });
+
+    test("returns false if the value is not a key in the object", function() {
+      expect(utils.contains({foo: false, bar: "bar"}, "quux")).toBe(false);
+      expect(utils.contains({foo: false, bar: "bar"}, null)).toBe(false);
+      expect(utils.contains({foo: false, bar: "bar"}, 1)).toBe(false);
+      expect(utils.contains({foo: false, bar: "bar"}, true)).toBe(false);
+    });
+
+    test("works with empty objects", function() {
+      expect(utils.contains({}, "foo")).toBe(false);
+    });
+  });
+});
+
+describe("unique", function() {
+  test("handles empty and single value arrays", function() {
+    expect(utils.unique(null)).toEqual(null);
+    expect(utils.unique([])).toEqual([]);
+    expect(utils.unique([1])).toEqual([1]);
+    expect(utils.unique(["foo"])).toEqual(["foo"]);
+    expect(utils.unique("foo")).toEqual("foo");
+  });
+
+  test("filters non unique values", function() {
+    expect(utils.unique(["foo", "bar", "bar", "foo", "baz"]))
+      .toEqual(["foo", "bar", "baz"]);
+
+    expect(utils.unique(["foo", "foo", "foo"]))
+      .toEqual(["foo"]);
+
+    expect(utils.unique([1, 2, 3, 3, 2, 1]))
+      .toEqual([1, 2, 3]);
+  });
+
+  test("returns a copy", function() {
+    const a = ["foo"];
+    expect(utils.unique(a)).not.toBe(a);
+  });
+});
+
+describe('resolveAttribute', () => {
+  test(`return normalized attribute\'s path array suitable for immutable setIn/getIn 
+  methods from a given string`, () => {
     expect(utils.resolveAttribute('foo.bar')).toEqual(['foo', 'bar']);
     expect(utils.resolveAttribute('foo.bar[0][1][2]')).toEqual(['foo', 'bar', 0, 1, 2]);
   });
--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,4 @@
-import { Iterable } from 'immutable';
 import moment from 'moment';
-
-class DeferredString extends String {
-  constructor(func, bindings) {
-    super();
-
-    this.func = func;
-    this.bindings = bindings;
-  }
-
-  toString() {
-    return this.func(this.bindings);
-  }
-}
 
 const utils = {
   EMPTY_STRING_REGEXP: /^\s*$/,
@@ -25,50 +11,50 @@ const utils = {
     return {}.toString.call(value) === '[object Array]';
   },
 
-    // Checks if the value is a number. This function does not consider NaN a
-    // number like many other `isNumber` functions do.
+  // Checks if the value is a number. This function does not consider NaN a
+  // number like many other `isNumber` functions do.
   isNumber(value) {
     return typeof value === 'number' && !isNaN(value);
   },
 
-    // Returns false if the object is not a function
+  // Returns false if the object is not a function
   isFunction(value) {
     return typeof value === 'function';
   },
 
-    // A simple check to verify that the value is an integer. Uses `isNumber`
-    // and a simple modulo check.
+  // A simple check to verify that the value is an integer. Uses `isNumber`
+  // and a simple modulo check.
   isInteger(value) {
     return this.isNumber(value) && value % 1 === 0;
   },
 
-    // Checks if the value is a boolean
+  // Checks if the value is a boolean
   isBoolean(value) {
     return typeof value === 'boolean';
   },
 
-    // Uses the `Object` function to check if the given argument is an object.
+  // Uses the `Object` function to check if the given argument is an object.
   isObject(obj) {
     return obj === Object(obj);
   },
 
-    // Simply checks if the object is an instance of a date
+  // Simply checks if the object is an instance of a date
   isDate(obj) {
     return obj instanceof Date;
   },
 
-    // Simply checks if the object is an instance of a date
+  // Simply checks if the object is an instance of a moment
   isMoment(obj) {
     return obj instanceof moment;
   },
 
-    // Returns false if the object is `null` of `undefined`
+  // Returns false if the object is `null` of `undefined`
   isDefined(obj) {
     return obj !== null && obj !== undefined;
   },
 
-    // Checks if the given argument is a promise. Anything with a `then`
-    // function is considered a promise.
+  // Checks if the given argument is a promise. Anything with a `then`
+  // function is considered a promise.
   isPromise(p) {
     return !!p && this.isFunction(p.then);
   },
@@ -86,47 +72,47 @@ const utils = {
       return true;
     }
 
-        // http://stackoverflow.com/a/384380/699304
-        /* istanbul ignore else */
+    // http://stackoverflow.com/a/384380/699304
+    /* istanbul ignore else */
     if (typeof HTMLElement === 'object') {
       return o instanceof HTMLElement;
     }
     return o &&
-                typeof o === 'object' &&
-                o !== null &&
-                o.nodeType === 1 &&
-                typeof o.nodeName === 'string';
+      typeof o === 'object' &&
+      o !== null &&
+      o.nodeType === 1 &&
+      typeof o.nodeName === 'string';
   },
 
   isEmpty(value) {
     let attr;
 
-        // Null and undefined are empty
+    // Null and undefined are empty
     if (!this.isDefined(value)) {
       return true;
     }
 
-        // functions are non empty
+    // functions are non empty
     if (this.isFunction(value)) {
       return false;
     }
 
-        // Whitespace only strings are empty
+    // Whitespace only strings are empty
     if (this.isString(value)) {
       return this.EMPTY_STRING_REGEXP.test(value);
     }
 
-        // For arrays we use the length property
+    // For arrays we use the length property
     if (this.isArray(value)) {
       return value.length === 0;
     }
 
-        // Dates have no attributes but aren't empty
+    // Dates have no attributes but aren't empty
     if (this.isDate(value)) {
       return false;
     }
 
-        // If we find at least one property we consider it non empty
+    // If we find at least one property we consider it non empty
     if (this.isObject(value)) {
       for (attr in value) {
         return false;
@@ -137,8 +123,8 @@ const utils = {
     return false;
   },
 
-    // Checks if the object is a hash, which is equivalent to an object that
-    // is neither an array nor a function.
+  // Checks if the object is a hash, which is equivalent to an object that
+  // is neither an array nor a function.
   isHash(value) {
     return this.isObject(value) && !this.isArray(value) && !this.isFunction(value);
   },
@@ -158,18 +144,6 @@ const utils = {
       return array;
     }
     return array.filter((el, index, array) => array.indexOf(el) === index);
-  },
-
-  formatMessage(message, bindings) {
-    if (this.isFunction(message)) {
-      message = new DeferredString(message, bindings);
-    } else {
-      Object.entries(bindings).forEach(([k, v]) => {
-        message = message.replace(new RegExp(`\\{${k}\\}`, 'g'), v);
-      });
-    }
-
-    return message;
   },
 
   resolveAttribute(attribute) {
@@ -198,12 +172,6 @@ const utils = {
     }
 
     return path;
-  },
-
-  getModelAttribute(model, attribute) {
-    const value = model.getIn(this.resolveAttribute(attribute));
-
-    return Iterable.isIterable(value) ? value.toJS() : value;
   },
 };
 
