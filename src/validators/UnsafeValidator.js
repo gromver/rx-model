533448c1b2a7a46d50195696aaf3fa18c4e4b192
Gayazov Roman <gromver5@gmail.com>
2017-08-22 18:13:16 +0300

    cleanup

--- src/validators/UnsafeValidator.js
+++ src/validators/UnsafeValidator.js
@@ -1,8 +1,5 @@
 import Validator from './Validator';
 
-/**
- * Наличие этого валидатора спорно, скорее всего он лишний, и будет убран
- */
 export default class UnsafeValidator extends Validator {
   isSafe() {
     return false;
